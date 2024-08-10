aa9a253893e8c4b00dff106a7bf4c252d61664a9
1024T <18046057402@189.cn>
2024-08-10 11:00:22 +0800

    Update run_comfyui.sh

--- run_comfyui.sh
+++ run_comfyui.sh
@@ -3,8 +3,8 @@ source /etc/network_turbo
 kill -9 `ps -ef|grep python | awk '{print $2}'`
 cd /root/ComfyUI
 source activate comfyui
-if [ ! -d /root/autodl-fs/models ]; then
-    cp -arf /root/models_back /root/autodl-fs/models
-    ln /root/autodl-fs/models -s models
+if [ ! -d /root/autodl-tmp/models ]; then
+    cp -arf /root/models_back /root/autodl-tmp/models
+    ln /root/autodl-tmp/models -s models
 fi
 python main.py --port 6006
